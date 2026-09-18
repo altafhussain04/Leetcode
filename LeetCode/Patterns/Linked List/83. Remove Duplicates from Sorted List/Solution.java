00d9d0111a747b344f5240efdbcbcb37d5272ceb
altafhussain04 <2k24.csai1a.2414174@gmail.com>
2026-09-18 16:37:01 +0530

    Add Easy Java Solution for 83. Remove Duplicates from Sorted List from LeetCode

--- LeetCode/Patterns/Linked List/83. Remove Duplicates from Sorted List/Solution.java
+++ LeetCode/Patterns/Linked List/83. Remove Duplicates from Sorted List/Solution.java
@@ -14,18 +14,17 @@ class Solution {
             return head;
         }
 
-        ListNode temp1=head;
-        while(temp1!=null && temp1.next!=null){
-            if(temp1.val==temp1.next.val){
-                temp1.next=temp1.next.next;
+        ListNode curr=head;
+        while(curr!=null && curr.next!=null){
+            if(curr.val==curr.next.val){
+                curr.next=curr.next.next;
             }
-
-           temp1=temp1.next;
-            
-           
+            else{
+                curr=curr.next;
+            }
         }
-
         return head;
+       
         
     }
 }
